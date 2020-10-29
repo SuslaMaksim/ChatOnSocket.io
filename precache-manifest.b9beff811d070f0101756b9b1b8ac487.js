a6fb4c162bbdafae5a2ae89cc49a22bfd638e868
SuslaMaksim <susla.maksim@gmail.com>
2020-10-29 15:41:47 +0200

    Updates

---
+++ precache-manifest.b9beff811d070f0101756b9b1b8ac487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45204c68218b3801273b62ab7025de59",
+    "url": "/SuslaMaksim/ChatOnSocket.io/index.html"
+  },
+  {
+    "revision": "019f7bc37e068ad0439a",
+    "url": "/SuslaMaksim/ChatOnSocket.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bedf8398dc570787e827",
+    "url": "/SuslaMaksim/ChatOnSocket.io/static/js/2.eafb1393.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/SuslaMaksim/ChatOnSocket.io/static/js/2.eafb1393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019f7bc37e068ad0439a",
+    "url": "/SuslaMaksim/ChatOnSocket.io/static/js/main.ea5c37dc.chunk.js"
+  },
+  {
+    "revision": "346125892d3ae682db2c",
+    "url": "/SuslaMaksim/ChatOnSocket.io/static/js/runtime-main.2ba09812.js"
+  }
+]);
